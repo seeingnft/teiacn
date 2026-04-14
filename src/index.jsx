6847569266315f6c9fcffbef08b2bb3526b94df1
seeingnft <86693809+seeingnft@users.noreply.github.com>
2026-04-14 23:14:41 +0800

    Add Chinese source files batch 4/4 - complete

---
+++ src/index.jsx
@@ -0,0 +1,118 @@
+import { RootErrorBoundary } from '@atoms/error/RootErrorBoundary'
+import { Tags } from '@pages/tags/index'
+import { About } from '@pages/about'
+import { Donate } from '@pages/donate'
+import { CollabDisplay } from '@pages/collaborate'
+import Text from '@pages/text'
+import Community from '@pages/text/Community'
+import OfficialPosts from '@pages/text/OfficialPosts'
+import { Settings } from '@pages/config/Settings'
+import { Subjkt } from '@pages/config/Subjkt'
+import { FAQ } from '@pages/faq'
+import { Home } from '@pages/home'
+import FriendsFeed from '@pages/home/feeds/friends-feed'
+import {
+  IranFeed, PakistanFeed, UkraineFeed, AudioFeed, GifFeed, GlbFeed,
+  HtmlSvgFeed, ImageFeed, VideoFeed, NewObjktsFeed, RandomFeed,
+  RecentSalesFeed, TagFeed, PdfFeed, MarkdownFeed, TextFeed,
+  QuakeFeed, MoroccoQuakeFeed, Tez4PalFeed, Art4ArtistsFeed,
+} from '@pages/home/feeds'
+import { ObjktDisplay } from '@pages/objkt-display'
+import { Info, Collectors, History } from '@pages/objkt-display/tabs'
+import Display from '@pages/profile'
+import Collections from '@pages/profile/collections'
+import Creations from '@pages/profile/creations'
+import Collabs from '@pages/profile/collabs'
+import Curation from '@pages/profile/curation'
+import TextPosts from '@pages/profile/text-posts'
+import { Terms } from '@pages/terms'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import './styles/index.scss'
+import { IconCache } from '@utils/with-icon'
+import { ListsFeed } from '@pages/home/feeds/lists-feed'
+import { MidiFeed } from '@pages/home/feeds/mime-type-feed'
+import { CodeOfConduct } from '@pages/codeofconduct'
+import { CoreValues } from '@pages/corevalues'
+import { PrivacyPolicy } from '@pages/privacypolicy'
+
+const display_routes = (
+  <>
+    <Route index element={<Creations />} />
+    <Route exact path="collection" element={<Collections />} />
+    <Route exact path="curation" element={<Curation />} />
+    <Route exact path="collabs" element={<Collabs />} />
+    <Route exact path="text" element={<TextPosts />} />
+  </>
+)
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="*" errorElement={<RootErrorBoundary />} element={<App />}>
+      <Route path="/*" index element={<Home />} />
+      <Route path="feed/*" element={<Home />}>
+        <Route path="sales" element={<RecentSalesFeed />} />
+        <Route path="lists" element={<ListsFeed />} />
+        <Route path="tezospride" element={<TagFeed tag="tezospride" namespace="tezospride" />} />
+        <Route path="iran" element={<IranFeed />} />
+        <Route path="tez4pal" element={<Tez4PalFeed />} />
+        <Route path="art4artists" element={<Art4ArtistsFeed />} />
+        <Route path="morocco-quake-aid" element={<MoroccoQuakeFeed />} />
+        <Route path="quake-aid" element={<QuakeFeed />} />
+        <Route path="pakistan" element={<PakistanFeed />} />
+        <Route path="ukraine" element={<UkraineFeed />} />
+        <Route path="random" element={<RandomFeed />} />
+        <Route path="newobjkts" element={<NewObjktsFeed />} />
+        <Route path="glb" element={<GlbFeed />} />
+        <Route path="video" element={<VideoFeed />} />
+        <Route path="image" element={<ImageFeed />} />
+        <Route path="audio" element={<AudioFeed />} />
+        <Route path="html-svg" element={<HtmlSvgFeed />} />
+        <Route path="pdf" element={<PdfFeed />} />
+        <Route path="md" element={<MarkdownFeed />} />
+        <Route path="txt" element={<TextFeed />} />
+        <Route path="midi" element={<MidiFeed />} />
+        <Route path="gif" element={<GifFeed />} />
+        <Route path="friends/:address" element={<FriendsFeed />} />
+      </Route>
+      <Route path="search/*" element={<Home isSearch />} />
+      <Route path="kt/:address" element={<Display />}>
+        <Route index element={<CollabDisplay />} />
+      </Route>
+      <Route path="collab/:name" element={<Display />}>
+        <Route index element={<CollabDisplay />} />
+      </Route>
+      <Route exact path="about" element={<About />} />
+      <Route path="donate/*" element={<Donate />} />
+      <Route exact path="terms" element={<Terms />} />
+      <Route exact path="faq" element={<FAQ />} />
+      <Route exact path="codeofconduct" element={<CodeOfConduct />} />
+      <Route exact path="corevalues" element={<CoreValues />} />
+      <Route exact path="privacypolicy" element={<PrivacyPolicy />} />
+      <Route path="text/*" element={<Text />}>
+        <Route index element={<Community />} />
+        <Route path="bulletin" element={<OfficialPosts />} />
+      </Route>
+      <Route path="objkt/:id/*" element={<ObjktDisplay />}>
+        <Route index element={<Info />} />
+        <Route path="listings" element={<Collectors />} />
+        <Route path="history" element={<History />} />
+      </Route>
+      <Route path="subjkt/*" element={<Subjkt />} />
+      <Route path="settings/*" element={<Settings />} />
+      <Route path="tags/:tag" element={<Tags />} />
+      <Route path="tz/:address/*" element={<Display />}>{display_routes}</Route>
+      <Route path=":name/*" element={<Display />}>{display_routes}</Route>
+    </Route>
+  )
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <IconCache.Provider value={{}}>
+    <RouterProvider router={router} />
+  </IconCache.Provider>
+)
+
+serviceWorker.unregister()
---
+++ vite.config.js
@@ -0,0 +1,99 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import viteTsconfigPaths from 'vite-tsconfig-paths'
+import svgrPlugin from 'vite-plugin-svgr'
+import path from 'path'
+import eslintPlugin from 'vite-plugin-eslint'
+import { splitVendorChunkPlugin } from 'vite'
+import { copySync } from 'fs-extra'
+import rollupNodePolyFill from 'rollup-plugin-polyfill-node'
+import mdPlugin from 'vite-plugin-markdown'
+import child_process from 'child_process'
+import { ViteEjsPlugin } from 'vite-plugin-ejs'
+import filterReplace from 'vite-plugin-filter-replace'
+
+const commitHash = child_process.execSync('git rev-parse HEAD').toString().trim()
+
+const copyPdfData = () => {
+  const prod = process.env.NODE_ENV === 'production'
+  const pdfjsDistPath = path.dirname(require.resolve('pdfjs-dist/package.json'))
+  const pdfWorkerPath = path.join(pdfjsDistPath, 'build', 'pdf.worker.min.js')
+  const cMapsDir = path.join(pdfjsDistPath, 'cmaps')
+  const standardFontsDir = path.join(pdfjsDistPath, 'standard_fonts')
+  const copy_data = (root = '') => {
+    copySync(pdfWorkerPath, `${root}pdf.worker.min.js`, { overwrite: true })
+    copySync(cMapsDir, `${root}cmaps/`, { overwrite: true })
+    copySync(standardFontsDir, `${root}standard_fonts/`, { overwrite: true })
+  }
+  return {
+    name: 'vite-plugin-copy-react-pdf',
+    buildStart: !prod ? () => { copy_data('public/') } : null,
+    writeBundle: prod ? () => { copy_data('build/') } : null,
+  }
+}
+
+const teiaAliases = {
+  '@atoms': path.resolve(__dirname, 'src', 'atoms'),
+  '@components': path.resolve(__dirname, 'src', 'components'),
+  '@constants': path.resolve(__dirname, 'src', 'constants'),
+  '@context': path.resolve(__dirname, 'src', 'context'),
+  '@data': path.resolve(__dirname, 'src', 'data'),
+  '@pages': path.resolve(__dirname, 'src', 'pages'),
+  '@hooks': path.resolve(__dirname, 'src', 'hooks'),
+  '@icons': path.resolve(__dirname, 'src', 'icons'),
+  '@styles': path.resolve(__dirname, 'src', 'styles'),
+  '@style': './index.module.scss',
+  '@utils': path.resolve(__dirname, 'src', 'utils'),
+  '@types': path.resolve(__dirname, 'src', 'types.d.ts'),
+  '../core/compat/globalThis': 'node_modules/@magenta/music/esm/core/compat/global.js',
+  './evaluate-audio-worklet-globalThis-scope-function': 'node_modules/standardized-audio-context/build/es2019/types/evaluate-audio-worklet-global-scope-function.js'
+}
+
+export default defineConfig(({ mode }) => {
+  const prod = mode === 'production'
+  let prod_plugs = []
+  if (prod) prod_plugs.push(eslintPlugin())
+  return {
+    base: process.env.GH_BASE_URL || '/',
+    clearScreen: prod,
+    appType: 'mpa',
+    plugins: [
+      ...prod_plugs,
+      react(),
+      splitVendorChunkPlugin(),
+      viteTsconfigPaths(),
+      svgrPlugin(),
+      mdPlugin({ mode: 'react' }),
+      ViteEjsPlugin({ BUILD_COMMIT: commitHash }),
+    ],
+    define: { global: 'globalThis' },
+    server: { host: true, port: 3000 },
+    build: {
+      commonjsOptions: { transformMixedEsModules: true },
+      outDir: 'build',
+      sourcemap: !prod,
+      rollupOptions: {
+        plugins: [rollupNodePolyFill()],
+        output: {
+          manualChunks: {
+            contracts: ['@taquito/beacon-wallet','@taquito/michelson-encoder','@stablelib/ed25519','@stablelib/nacl','@stablelib/x25519-session','@taquito/taquito'],
+            pdf: ['react-pdf', 'pdfjs-dist'],
+            magenta: ['@magenta/music','@tensorflow/tfjs-core','@tensorflow/tfjs-layers','@tensorflow/tfjs-converter','@tensorflow/tfjs-data','@tensorflow/tfjs-backend-cpu','@tensorflow/tfjs-backend-webgl'],
+            midi: ['@tonejs/midi', 'html-midi-player'],
+            ui: ['classnames','prop-types','react','react-router-dom','react-dom','framer-motion','lodash','zustand','axios','graphql','graphql-request','swr'],
+          },
+        },
+      },
+    },
+    optimizeDeps: { commonjsOptions: { transformMixedEsModules: true }, esbuildOptions: {} },
+    resolve: {
+      alias: {
+        'readable-stream': 'vite-compatible-readable-stream',
+        stream: 'vite-compatible-readable-stream',
+        path: require.resolve('path-browserify'),
+        util: 'rollup-plugin-node-polyfills/polyfills/util',
+        ...teiaAliases,
+      },
+    },
+  }
+})
